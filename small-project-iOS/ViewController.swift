b7ebcc02a0035f0945865789f1bb3b2db3f1404f
Riccardo Menon <ric.menon2000@gmail.com>
2022-07-06 10:02:38 +1000

    initialised empty project

---
+++ small-project-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  small-project-iOS
+//
+//  Created by AUCTIONS LIVE on 6/7/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
